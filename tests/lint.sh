36b21913889b2ecbf9bdb5044be71c803fff8499
Craig Duncan <git@duncanc.co.uk>
2014-11-16 17:56:15 +0000

    Added a linting check for all souce files

---
+++ tests/lint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+status=0
+
+cd $(dirname $0)/..
+files=$(find src -name "*.php")
+files="$files bin/sonos"
+for file in $files; do
+    php -l $file
+    if [[ $? > 0 ]]; then
+        status=255
+    fi
+done
+
+exit $status
